8db704ecbf6e8be0ccef171e9a1e8f5579909b9e
Inna <inna-asbest@mail.ru>
2022-08-19 16:01:28 +0500

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
+    // Задание 1
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Иван ";
+        String middleName = "Иванович";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        System.out.println();
+
+
+        // Задание 2
+
+        String fullNameUpper = fullName.toUpperCase();
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameUpper);
+        System.out.println();
+
+
+        // Задание 3
+
+        String fullNameI = "Иванов Семён Семёнович";
+        fullNameI = fullNameI.replace("ё", "e");
+        System.out.println("Данные ФИО сотрудника -" + fullNameI);
+
+
     }
 }
+
+
+
+
+
+
